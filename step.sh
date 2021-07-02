f996b5b5c10f0be7198f7b7cf3486d3501825743
Marlon Tojal <marlont@backbase.com>
2021-07-02 10:11:25 +0200

    fix if

--- step.sh
+++ step.sh
@@ -15,15 +15,15 @@ envman add --key BD_HUB_TOKEN --value $blackduck_token
 envman add --key VERACODE_ID --value $veracode_id
 envman add --key VERACODE_KEY --value $veracode_key
 
-if [ -z "$signing_path" ]; then
+if [[ ! -z "$signing_path" ]]; then
   options="$options --signArtifacts $signing_path"
 fi
 
 if [[ $project_type == 'android' ]]; then
-  if [ -z "$gradle_project" ]; then
+  if [[ ! -z "$gradle_project" ]]; then
     options="$options --detectGradleProject $gradle_project"
   fi
-  if [ -z "$gradle_configuration" ]; then
+  if [[ ! -z "$gradle_configuration" ]]; then
     options="$options --detectGradleConfiguration $gradle_configuration"
   fi
 fi
